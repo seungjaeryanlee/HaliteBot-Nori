5dde988b20e7f354ce35dc85931797b61297711f
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2016-12-26 22:00:02 -0500

    Create Nori v2
    Doesn't move if below certain ratio of strength / production. Logs total strength.

--- NorivRandom.sh
+++ NorivRandom.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-g++ -std=c++11 NoriBot.cpp -o NoriBot.o
+g++ -std=c++11 NoriBotv2.cpp -o NoriBotv2.o
 g++ -std=c++11 RandomBot.cpp -o RandomBot.o
-./halite -d "30 30" "./NoriBot.o" "./RandomBot.o"
+./halite -d "30 30" "./NoriBotv2.o" "./RandomBot.o"
+rm NoriBotv2.o
+rm RandomBot.o
