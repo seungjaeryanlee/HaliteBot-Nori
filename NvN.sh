7299f655f5df26c49fbf2bcab7147385112608dd
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2016-12-27 12:03:55 -0500

    Create Nori v6
    Bot now waits on border if it cannot defeat adjacent block

--- NvN.sh
+++ NvN.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+g++ -std=c++11 Noriv6.cpp -o Nori6.o
 g++ -std=c++11 Noriv5.cpp -o Nori5.o
-g++ -std=c++11 Noriv4.cpp -o Nori4.o
-./halite -d "30 30" "./Nori5.o" "./Nori4.o"
+./halite -d "30 30" "./Nori6.o" "./Nori5.o"
+rm Nori6.o
 rm Nori5.o
-rm Nori4.o
