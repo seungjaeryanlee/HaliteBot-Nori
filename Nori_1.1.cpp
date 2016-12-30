85f1f9e394095ba2bba443dc443597ed95184b5f
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2016-12-30 14:19:03 -0500

    Create Nori 1.1
    Disable 50% chance to stay still for big blocks

---
+++ Nori_1.1.cpp
@@ -0,0 +1,103 @@
+#include <stdlib.h>
+#include <time.h>
+#include <cstdlib>
+#include <ctime>
+#include <time.h>
+#include <set>
+#include <fstream>
+
+#include "hlt.hpp"
+#include "networking.hpp"
+
+const int MIN_RATIO = 5;
+
+typedef unsigned char Direction;
+
+Direction findNearestBorder(hlt::GameMap& map, const hlt::Location& loc, const unsigned char ID) {
+    int maxDistance = (map.width < map.height) ? map.width/2 : map.height/2;
+    Direction bestDir = NORTH;
+
+    for(Direction dir : CARDINALS) {
+        unsigned short distance = 0;
+        hlt::Location current = loc;
+        hlt::Site site = map.getSite(current, dir);
+        while(site.owner == ID && distance < maxDistance) {
+            distance++;
+            current = map.getLocation(current, dir);
+            site = map.getSite(current);
+        }
+
+        if(distance < maxDistance) {
+            bestDir = dir;
+            maxDistance = distance;
+        }
+    }
+
+    return bestDir;
+}
+
+hlt::Move getMove(hlt::GameMap& map, const hlt::Location& loc, const unsigned char ID) {
+    bool isBorder = false;
+
+    // If on border, wait until you can attack
+    for(Direction dir : CARDINALS) {
+        const hlt::Site neighborSite = map.getSite(loc, dir);
+        if (neighborSite.owner != ID) {
+            isBorder = true;
+            if(neighborSite.strength < map.getSite(loc).strength) {
+                return {loc, dir};
+            }
+        }
+    }
+    if(isBorder) {
+        return {loc, STILL};
+    }
+
+    // If below certain ratio of strength/production, don't move
+    if(map.getSite(loc).strength < map.getSite(loc).production * MIN_RATIO) {
+        return {loc, STILL};
+    }
+
+    // If not too large, 1/2 chance to stay still
+    if(map.getSite(loc).strength < 100 && rand() % 2) {
+        return {loc, STILL};
+    }
+
+    // Move towards border
+    return {loc, findNearestBorder(map, loc, ID)};
+}
+
+int main() {
+
+    srand(time(NULL));
+
+    std::ofstream log;
+    log.open("nori.log");
+
+    std::cout.sync_with_stdio(0);
+
+    unsigned char myID;
+    hlt::GameMap presentMap;
+    getInit(myID, presentMap);
+    sendInit("Nori 1.1");
+
+    std::set<hlt::Move> moves;
+    for(int i = 1; ; i++) {
+        moves.clear();
+
+        getFrame(presentMap);
+
+        for(unsigned short a = 0; a < presentMap.height; a++) {
+            for(unsigned short b = 0; b < presentMap.width; b++) {
+                if (presentMap.getSite({ b, a }).owner == myID) {
+                    moves.insert(getMove(presentMap, {b, a}, myID));
+                }
+            }
+        }
+
+        sendFrame(moves);   
+    }
+
+    log.close();
+    return 0;
+}
--- NvN.sh
+++ NvN.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+g++ -std=c++11 Nori_1.1.cpp -o Nori_1.1.o
 g++ -std=c++11 Nori_1.0.cpp -o Nori_1.0.o
-g++ -std=c++11 Nori_0.6.cpp -o Nori_0.6.o
-./halite -d "30 30" "./Nori_1.0.o" "./Nori_0.6.o"
+./halite -d "30 30" "./Nori_1.1.o" "./Nori_1.0.o"
+rm Nori_1.1.o
 rm Nori_1.0.o
-rm Nori_0.6.o
