ba8c0a4303dcdfe2edd72342f3902033ebcf1303
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2017-01-02 16:53:17 -0500

    Clean up unused files

--- NvN.sh
+++ NvN.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-g++ -std=c++11 Nori_2.1.cpp -o Nori_2.1.o
-g++ -std=c++11 Nori_2.0.cpp -o Nori_2.0.o
-./halite -d "30 30" "./Nori_2.1.o" "./Nori_2.0.o"
+g++ -std=c++11 Nori_2.1.cpp -o Nori_2.2.o
+g++ -std=c++11 Nori_2.0.cpp -o Nori_2.1.o
+./halite -d "30 30" "./Nori_2.2.o" "./Nori_2.1.o"
+rm Nori_2.2.o
 rm Nori_2.1.o
-rm Nori_2.0.o
--- NvR.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-g++ -std=c++11 Nori_1.0.cpp -o Nori_1.0.o
-g++ -std=c++11 RandomBot.cpp -o RandomBot.o
-./halite -d "30 30" "./Nori_1.0.o" "./RandomBot.o"
-rm Nori_1.0.o
-rm RandomBot.o
