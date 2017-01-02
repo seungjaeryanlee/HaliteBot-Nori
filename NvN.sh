e6e758c598d8802cd55544eb64fdcbaf6e0071b3
Seung Jae (Ryan) Lee <sjlee235@gmail.com>
2017-01-02 17:48:40 -0500

    Organize code

--- NvN.sh
+++ NvN.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-g++ -std=c++11 Nori_2.1.cpp -o Nori_2.2.o
-g++ -std=c++11 Nori_2.0.cpp -o Nori_2.1.o
-./halite -d "30 30" "./Nori_2.2.o" "./Nori_2.1.o"
-rm Nori_2.2.o
+g++ -std=c++11 Nori_3.cpp -o Nori_3.o
+g++ -std=c++11 Nori_2.1.cpp -o Nori_2.1.o
+./halite -d "30 30" "./Nori_3.o" "./Nori_2.1.o"
+rm Nori_3.o
 rm Nori_2.1.o
